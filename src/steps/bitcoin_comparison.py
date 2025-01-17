18d909d218281e8b860621de9236af42694cf3a5
Daniel Jones <danielkjones0@gmail.com>
2025-01-17 03:06:08 -0500

    draft of bitcoin comparison completed

---
+++ src/steps/bitcoin_comparison.py
@@ -0,0 +1,116 @@
+import ast
+import json
+from os.path import dirname, join
+
+import pandas as pd
+
+
+class BitcoinComparisonStep:
+
+    def __init__(self, timestamp: str):
+        self.timestamp = timestamp
+
+        self.pricing_file_directory = join(
+            dirname(dirname(dirname(__file__))), "data_lake/pricing"
+        )
+        self.pricing_file_format = "coins_pricing_{}.csv"
+        self.listings_file_directory = join(
+            dirname(dirname(dirname(__file__))), "data_lake/listings"
+        )
+
+        self.listings_file_format = "crypto_listings_{}.csv"
+        self.bitcoin_comparison_directory = join(
+            dirname(dirname(dirname(__file__))), "data_lake/bitcoin_comparison"
+        )
+        self.bitcoin_comparison_file_format = "bitcoin_comparison_{}.csv"
+
+    def generate_bitcoin_comparison(self) -> pd.DataFrame:
+        pricing_df = self.read_pricing()
+        pricing_df["percent_change_24h"] = pricing_df["quote"].apply(
+            self.unpack_percent_change_24h
+        )
+
+        bitcoin_percetage_change = self.bitcoin_percent_change_24h()
+        pricing_df["bitcoin_percent_change_24h"] = bitcoin_percetage_change
+
+        # Finding difference. NEGATIVE value means that the coin changed LESS than bitcoin. POSITIVE value means
+        # the coin changed MORE than bitcoin.
+        pricing_df["24h_against_bitcoin"] = (
+            pricing_df["percent_change_24h"] - pricing_df["bitcoin_percent_change_24h"]
+        )
+
+        comparison_df = self.trim_df_values(pricing_df)
+        sorted_df = self.sort_df(comparison_df)
+
+        self.write_df(sorted_df)
+        return sorted_df
+
+    def read_pricing(self) -> pd.DataFrame:
+        df = pd.read_csv(
+            join(
+                self.pricing_file_directory,
+                self.pricing_file_format.format(self.timestamp),
+            )
+        )
+        return df
+
+    def trim_df_values(self, comparison_df: pd.DataFrame) -> pd.DataFrame:
+        """There are only a few values that we want to have in this dataset
+        to avoid cluttering for the end user
+
+        Returns:
+            pd.DataFrame: DataFrame with extraneous information removed
+        """
+        return comparison_df[
+            [
+                "symbol",
+                "name",
+                "24h_against_bitcoin",
+                "percent_change_24h",
+                "bitcoin_percent_change_24h",
+                "LoadedWhen",
+            ]
+        ]
+
+    def sort_df(self, comparison_df: pd.DataFrame) -> pd.DataFrame:
+        """Need to sort the dataframe from smallest difference to largest
+        difference
+
+        Args:
+            comparison_df (pd.DataFrame): Unsorted Comparison DataFrame
+
+        Returns:
+            pd.DataFrame: DF Sorted by the difference from smallest to largest
+        """
+        return comparison_df.sort_values(
+            by="24h_against_bitcoin", ascending=True
+        ).reset_index(drop=True)
+
+    def write_df(self, comparison_df: pd.DataFrame) -> None:
+        dataset_path = join(
+            self.bitcoin_comparison_directory,
+            self.bitcoin_comparison_file_format.format(self.timestamp),
+        )
+        comparison_df.to_csv(dataset_path)
+
+    def bitcoin_percent_change_24h(self) -> float:
+        # Need to get the value of bitcoin from one of the other layers
+        # It may be worthwhile modifying in the near future to have a
+        # clean set of listings... TBD
+        listing_df = pd.read_csv(
+            join(
+                self.listings_file_directory,
+                self.listings_file_format.format(self.timestamp),
+            )
+        )
+        # get the bitcoin quote from the listing df
+        bitcoin_quote = listing_df.loc[listing_df["name"] == "Bitcoin"]["quote"][0]
+        bitcoin_percent_change_24h = self.unpack_percent_change_24h(bitcoin_quote)
+        return bitcoin_percent_change_24h
+
+    def unpack_percent_change_24h(self, quote: str):
+        # Unpack price information
+        # Need to use ast due to the formatting constraints of pandas .to_csv.
+        # May be worth investigating a better way in the future
+        jsn = ast.literal_eval(quote)
+        return jsn.get("USD").get("percent_change_24h")
---
+++ test/steps/test_bitcoin_comparison.py
@@ -0,0 +1,50 @@
+import os
+from os.path import dirname, join
+
+import pytest
+
+from src.steps.bitcoin_comparison import BitcoinComparisonStep
+
+TEST_TIMESTAMP = "20250116000000"
+TEST_PRICING_DIRECTORY = join(dirname(dirname(__file__)), "mock_data_lake/pricing")
+TEST_LISTINGS_DIRECTORY = join(dirname(dirname(__file__)), "mock_data_lake/listings")
+TEST_BITCOIN_COMPARISON_DIRECTORY = join(
+    dirname(dirname(__file__)), "temp_data_lake/bitcoin_comparison"
+)
+
+
+def delete_directory_contents(directory: str) -> None:
+    """Delete the contents of a given directory
+    TODO should probably move this to be a test helper that can be used in multiple locations
+
+    Args:
+        directory (str): Directory to delete files from
+    """
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+
+# TODO could probably consolidate all cleanup / teardown. We'll see
+@pytest.fixture
+def clean_test_directory():
+    delete_directory_contents(TEST_BITCOIN_COMPARISON_DIRECTORY)
+    yield
+    delete_directory_contents(TEST_BITCOIN_COMPARISON_DIRECTORY)
+
+
+class TestBitcoinComparison:
+
+    def test_unpack_percent_change_24h(self):
+        example_quote_string = "{'USD': {'price': 99203.96568075211, 'volume_24h': 58240590207.8066, 'volume_change_24h': 15.2907, 'percent_change_1h': 0.15811113, 'percent_change_24h': 2.76610597, 'percent_change_7d': 5.98901655, 'percent_change_30d': -7.17412121, 'percent_change_60d': 9.20697253, 'percent_change_90d': 46.59964611, 'market_cap': 1965320141316.709, 'market_cap_dominance': 56.0444, 'fully_diluted_market_cap': 2083283279295.79, 'tvl': None, 'last_updated': '2025-01-16T12:31:00.000Z'}}"
+        step = BitcoinComparisonStep(TEST_TIMESTAMP)
+        percent_change = step.unpack_percent_change_24h(example_quote_string)
+        assert percent_change == 2.76610597
+
+    def test_generate_bitcoin_comparison(self, clean_test_directory):
+        step = BitcoinComparisonStep(TEST_TIMESTAMP)
+        step.pricing_file_directory = TEST_PRICING_DIRECTORY
+        step.listings_file_directory = TEST_LISTINGS_DIRECTORY
+        step.bitcoin_comparison_directory = TEST_BITCOIN_COMPARISON_DIRECTORY
+        df = step.generate_bitcoin_comparison()
